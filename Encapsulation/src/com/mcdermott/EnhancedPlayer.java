b1c501e4a9bdd626b5c3d54f96489641894bc21d
Brian McDermott <Brian McDermott>
2016-07-21 15:20:04 -0400

    Added Encapsulation exercise

---
+++ Encapsulation/src/com/mcdermott/EnhancedPlayer.java
@@ -0,0 +1,30 @@
+package com.mcdermott;
+
+public class EnhancedPlayer {
+	private String name;
+	private int hitPoints = 100;
+	private String weapon;
+
+	public EnhancedPlayer(String name, int health, String weapon) {
+		super();
+		this.name = name;
+		if (health > 0 && health <= 100) {
+			this.hitPoints = health;
+		}
+		this.weapon = weapon;
+	}
+	
+	public void loseHealth(int damage){
+		this.hitPoints = this.hitPoints - damage;
+		if(this.hitPoints <=0){
+			System.out.println("Player knocked out");
+			//Reduce number of lives remaining for the player
+		}
+	}
+		
+		public int getHealth() {
+			return hitPoints;
+		}
+
+
+}
